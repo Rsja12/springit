a36b3902016fdd5ab5db675a4a2162357d4f7ce7
Rsja12 <asocarras10@gmail.com>
2020-12-26 13:51:05 -0500

    Refactor Comment & Vote classes to use Lombok annotations for generating Getters & Setters, toString, equalsAndHashCode, RequiredArgsConstructor & NoArgsConstructor

--- src/main/java/com/oreilly/springit/model/Vote.java
+++ src/main/java/com/oreilly/springit/model/Vote.java
@@ -1,40 +1,19 @@
 package com.oreilly.springit.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Vote {
 
     @Id
     @GeneratedValue
     private Long id;
     private Integer vote;
-
-    public Vote(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getVote() {
-        return vote;
-    }
-
-    public void setVote(Integer vote) {
-        this.vote = vote;
-    }
-
-    @Override
-    public String toString() {
-        return "Vote{" +
-                "id=" + id +
-                ", vote=" + vote +
-                '}';
-    }
 }
