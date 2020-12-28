218295398ee0a6b9ad0482f555d842706e373f40
Rsja12 <asocarras10@gmail.com>
2020-12-28 16:46:01 -0500

    Create home template using Thymeleaf. Refactor HomeController's home method to display Thymeleaf template

--- src/main/java/com/oreilly/springit/controller/HomeController.java
+++ src/main/java/com/oreilly/springit/controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.oreilly.springit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Home Page";
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Hello, Thymeleaf!");
+        return "home";
     }
 
 }
