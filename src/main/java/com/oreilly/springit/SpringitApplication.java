69c98524841fb0437461b84616ff0ae2ab1f3666
Rsja12 <asocarras10@gmail.com>
2020-12-29 08:34:55 -0500

    Add PrettyTime Bean in SpringitApplication class. Create BeanUtil service. Add prettyTime methods to Link domain class

--- src/main/java/com/oreilly/springit/SpringitApplication.java
+++ src/main/java/com/oreilly/springit/SpringitApplication.java
@@ -1,7 +1,9 @@
 package com.oreilly.springit;
 
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,5 +14,10 @@ public class SpringitApplication {
 		SpringApplication.run(SpringitApplication.class, args);
 
 		System.out.println("Welcome to Springit!");
+	}
+
+	@Bean
+	PrettyTime prettyTime() {
+		return new PrettyTime();
 	}
 }
