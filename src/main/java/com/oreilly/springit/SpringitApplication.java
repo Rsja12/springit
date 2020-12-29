0163db34c9e27a7b95728794b18f3655047bb667
Rsja12 <asocarras10@gmail.com>
2020-12-29 08:11:43 -0500

    Remove unnecessary imports from SpringitApplication class

--- src/main/java/com/oreilly/springit/SpringitApplication.java
+++ src/main/java/com/oreilly/springit/SpringitApplication.java
@@ -1,13 +1,7 @@
 package com.oreilly.springit;
 
-import com.oreilly.springit.domain.Comment;
-import com.oreilly.springit.domain.Link;
-import com.oreilly.springit.repository.CommentRepository;
-import com.oreilly.springit.repository.LinkRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
